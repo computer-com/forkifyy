7126ce05dd0a75ef1b1eedafc7e7cd5fa7517dc7
VIRENPANDYA <virenpandya1975@gmail.com>
2025-04-07 17:56:01 -0400

    fixed wrong wording

--- server/routes/restaurantRoutes.js
+++ server/routes/restaurantRoutes.js
@@ -4,7 +4,7 @@ const Restaurant = require('../models/Restaurant');
 const User = require('../models/User');
 const { auth, isAdmin, isOwner } = require('../middleware/auth');
 const router = express.Router();
-const bcrypt = require('bcryptjsjs');
+const bcrypt = require('bcryptjs');
 const slugify = require('slugify');
 
 // Configure multer for image upload
